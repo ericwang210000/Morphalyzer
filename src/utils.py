c6e66556f62c71242f3deedc72e4b7119c23aeea
Eric Wang <ericwang@MacBook-Air.local>
2025-04-05 05:41:30 -0500

    helper func in loss to get loss func

--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,11 @@ import torch
 import torch.nn.functional as F
 
 # compute the binary cross-entropy loss using logits from NN and tensor-encoded "real" images
-def get_loss(logits, targets):
+def loss_fn(logits, targets):
     loss_compute = torch.nn.BCEWithLogitsLoss()
     loss = loss_compute(logits, targets)
     return loss
+
+def get_loss():
+    return loss_fn
 
