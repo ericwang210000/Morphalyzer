5840dfd5fcbfe2cd1d5c81503841a17e6310eecc
Eric Wang <ericwang@MacBook-Air.local>
2025-04-05 04:18:47 -0500

    phase 1 model developed + architecture defined and illustrated

--- README.md
+++ README.md
@@ -18,7 +18,13 @@ With the release of *4o Image Generation* by OpenAI (25 March, 2025), proliferat
 
 **Phase 1** focuses on building a binary image classifier using pixel data. The goal is to distinguish between real and AI-generated facial images (e.g., CelebA vs. ThisPersonDoesNotExist). Later phases will incorporate non-pixel features such as metadata and byte-level compression traces. In this phase, pre-processed images will be used to train, validate and test (70/15/15) the model. Training will be done using the Adam optimizer with an inital learning rate of 0.001. This will act as the base architecture. 
 
-**Phase 2** will attempt to fuse CNN-derived feature maps with independent file-level features to improve classification robustness. Features like EXIF/tEXt metadata, entropy, compression artifacts, and byte frequency will be extracted and encoded into a fixed-length vector and injected into intermediate layers via skip connections (Mid-layer injection, DenseNet, Dual-branch network).
+**Phase 2** will attempt to fuse CNN-derived feature maps with independent file-level features to improve classification robustness. Features like EXIF/tEXt metadata, entropy, compression artifacts, and byte frequency will be extracted from an image file and encoded into a fixed-length vector. Skip connections will then be used to inject these low level features into intermediate layers and concatenate them with mid-level feature maps. (Mid-layer injection -> DenseNet -> Dual-branch network).
+
+## **Proposed NN Architecture:
+<p align="center">
+  <img src="assets/CNN.png" alt="Logo" width="300"/>
+</p>
+
 
 **Phase 3 and 4** will interpret and validate the model's performance across multiple real-world conditions. Demo deployment for public use may be a good way to do so. Evaluation methods are tentative.
 
--- models/resNetLite.py
+++ models/resNetLite.py
@@ -1,26 +1,15 @@
 import torch
 import torch.nn as nn
 
-# custom residual block class
 class ResidualBlock(nn.Module):
-    # initialize block, downsampling will occur explicitly externally
     def __init__(self, in_channels, out_channels, downsample=False):
-        # inheritance
         super(ResidualBlock, self).__init__()
-        # set stride
-        stride = None
-        if downsample:
-            stride = 2
-        else:
-            stride = 1
-        # apply 2 convolutional layers per level: convolution 3 x 3 + BatchNorm + ReLU
-        # convolution: kernel size 3 + padding 1 preserves spatial dimensions, bias is false bc proceeding BatchNorm
+        stride = 2 if downsample else 1
+
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, stride=stride, bias=False)
-        # normalize convolution output
         self.bn1 = nn.BatchNorm2d(out_channels)
-        # activation (non-linear)
         self.relu = nn.ReLU(inplace=True)
-        # 2nd convolution applied to output from first layer
+
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, stride=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
 
@@ -32,49 +21,42 @@ class ResidualBlock(nn.Module):
             )
         else:
             self.downsample = nn.Identity()
-    # forward pass
+
     def forward(self, x):
-        # identity path
         identity = self.downsample(x)
-        # main path
+
         out = self.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
-        # skip connection (addition:  main + identity)
+
         out += identity
         out = self.relu(out)
-        # final tensor
+
         return out
-# ResNet-based NN architecture class 
+
 class ResNetLite(nn.Module):
-    # only one class needed for sigmoid activation
     def __init__(self, num_classes=1):
         super(ResNetLite, self).__init__()
-        # 3 input channels (RGB) converted to 64 feature maps
+        
         self.stem = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True)
         )
-        # NN layers, increase feature maps when downsampling, otherwise don't
+
         self.layer1 = ResidualBlock(64, 64, downsample=False)
         self.layer2 = ResidualBlock(64, 128, downsample=True)
-        # reduce feature maps to single val and map to fully connected layer
+
         self.global_pool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(128, num_classes)
 
     def forward(self, x):
-        # encoder layer 1 (returns 64 feature maps)
         x = self.stem(x)
-        # encoder layer 2 (returns 64 feature maps)
         x = self.layer1(x)
-        # encoder layer 3 (returns 128 feature maps)
         x = self.layer2(x)
-        # reduce and flatten feature maps to 128 dim. tensor and transform into single raw logit
         x = self.global_pool(x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
-        # raw logits
-        return x 
+        return x  # raw logits (apply sigmoid externally if needed)
 
 def get_model(num_classes=1):
     return ResNetLite(num_classes=num_classes)
